727440050d6a5bc510ada999369b339aed4a1944
Sam Bulatov <stylesam@yandex.ru>
2020-06-06 01:53:51 +0300

    Добавлены методы filter и map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Observable } from './observable'
+/*
 
 const hello = new Observable((observer) => {
 
@@ -15,3 +16,26 @@ const hello = new Observable((observer) => {
 const sub = hello.subscribe({
   next: value => console.log(value)
 })
+*/
+
+
+function interval(intervalInMs: number) {
+  return new Observable<number>((observer) => {
+    let intervalCounter = 0
+
+    const intervalId = setInterval(() => {
+      observer.next(intervalCounter)
+      intervalCounter++
+    }, intervalInMs)
+
+    return () => clearInterval(intervalId)
+  })
+}
+
+const test$ = interval(1000).map(v => v * 2)
+
+const subscription = test$.subscribe({
+  next: value => console.log(value)
+})
+
+// setTimeout(() => subscription.unsubscribe(), 2000)
--- src/observable.ts
+++ src/observable.ts
@@ -2,12 +2,10 @@ import { EMPTY_OBSERVER, Subscriber, Observer } from './models'
 import { Subscription } from './subscription'
 
 export class Observable<T> {
-  constructor(private subscriber: Subscriber<T>) {}
 
-  public subscribe(observer: Observer<T>) {
-    const filledObserver = { ...EMPTY_OBSERVER, ...observer }
-    return new Subscription(filledObserver, this.subscriber)
-  }
+  public operators: Function[] = []
+
+  constructor(private subscriber: Subscriber<T>) {}
 
   static of<G>(...values: G[]) {
     return new Observable<G>((observer) => {
@@ -27,6 +25,52 @@ export class Observable<T> {
   }
 
   static from<G>(values: G[]) {
+
+  }
+
+  public subscribe(observer: Observer<T>) {
+    const filledObserver = { ...EMPTY_OBSERVER, ...observer }
+    return new Subscription(filledObserver, this.subscriber)
+  }
+
+  public filter(project: (value: T) => boolean) {
+    return new Observable<T>((observer) => {
+      const subscription = this.subscribe({
+        next: (value: T) => {
+          try {
+            if (project(value)) observer.next(value)
+          } catch (e) {
+            observer.error(e)
+          }
+        },
+        error: e => observer.error(e),
+        complete: () => observer.complete()
+      })
+
+      return () => subscription.unsubscribe()
+    })
+  }
+
+  public map<G>(project: (v: T) => G) {
+    return new Observable<G>(observer => {
+      const subscription = this.subscribe({
+        next: (value: T) => {
+          try {
+            const newVal = project(value)
+            observer.next(newVal)
+          } catch (e) {
+            observer.error(e)
+          }
+        },
+        error: e => observer.error(e),
+        complete: () => observer.complete()
+      })
+
+      return () => subscription.unsubscribe()
+    })
+  }
+
+  public reduce(project: (previous, current: T) => void, seed) {
 
   }
 }
